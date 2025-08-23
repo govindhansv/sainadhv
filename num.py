77c4281751bc87e610965856ddfae1dcb425ee0a
Govind Hans <govindhansv333@gmail.com>
2025-08-23 12:58:19 +0530

    hellp

--- num.py
+++ num.py
@@ -1,5 +1,13 @@
 import numpy    as np
+
 a=np.array([2,3,5,1])
 sorted_a=np.sort(a)
-print("Original array: "+str(a))
-print("Sorted array: "+str(sorted_a))
+print("Original array: ",a)
+print("Sorted array: ",sorted_a)
+
+print(np.zeros((3,4)))
+print(np.ones((2,3)))
+
+b=np.arange(1,11)
+print(b)
+
